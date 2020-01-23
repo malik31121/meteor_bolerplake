d11f7254bbb9241245102f5a45be47385cbf6a89
malik <malikhamana@gmail.com>
2020-01-23 11:04:10 +0100

    import react

--- client/index.js
+++ client/index.js
@@ -1,1 +1,11 @@
-console.log('CLIENT METEOR ET LANCER')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '/imports/routers/App'
+
+console.log('CLIENT : Meteor est lancé')
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
